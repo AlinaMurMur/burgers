3f2640897f6ddc1cdf70e3a33363610ec3f84171
AlinaMurMur <alinamurtazina777@yandex.ru>
2023-11-23 10:12:51 +0300

    path8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
+// @ts-ignore
+
 import {Component} from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
 import {AppService} from "./app.service";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'burgers';
   currency = '$';
